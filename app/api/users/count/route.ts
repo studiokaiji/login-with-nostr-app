efc32344c604149efd9371a256b02103ee299ba3
Haruki <nazawaharuki@gmail.com>
2023-07-07 07:59:52 +0900

    カウンターではなくzcountで集計するように変更

--- app/api/users/count/route.ts
+++ app/api/users/count/route.ts
@@ -3,7 +3,7 @@ import { kv } from "@vercel/kv";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_: NextRequest) {
-  const usersCount = await kv.get(USERS_COUNT_KEY);
+  const usersCount = await kv.zcount("users", "-inf", "+inf");
   if (typeof usersCount !== "number") {
     return NextResponse.json({ count: 0 }, { status: 200 });
   }
